13a3f2c1995ac0a1f943122c9e01ea65aacf8beb
mertdogramaci <mertdogramaci@gmail.com>
2022-08-08 22:50:14 +0300

    main menu has been added

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ document.getElementById("deck_size_button").onclick = function() {
         document.getElementById("deck_size_error").style.display = "none";
         document.getElementById("main_deck").style.display = "inline";
         document.getElementById("selected_card_title").style.display = "inline";
+        document.getElementById("input_section").style.display = "none";
+        document.getElementById("player_side").style.display = "inline";
 
         for (let i = 1; i <= deck_size; i++) {
             document.getElementById("main_" + i).style.display = "inline";
